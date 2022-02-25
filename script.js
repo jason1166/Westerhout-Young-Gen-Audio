20c01ad4406460bcdfbc977c87126376c60e15fe
jason1166 <78394632+jason1166@users.noreply.github.com>
2022-02-25 22:35:31 +0800

    Add files via Upload
    Added html, js, css files and image

---
+++ script.js
@@ -0,0 +1,138 @@
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            audio: null,
+            circleLeft: null,
+            barWidth: null,
+            duration: null,
+            currentTime: null,
+            isTimerPlaying: false,
+            tracks: [
+                {
+                    //temp holder
+                    name: "Young Generation of the Westerhout Family",
+                    artist: "Westerhout Family",
+                    cover: "",
+                    source: "https://cdn.glitch.global/1d6e4202-cbd8-4710-885c-80dc23bab095/Westerhout_Young_Gen.wav?v=1645797330102", 
+                    url: "",
+                    favorited: false
+                },
+            ],
+            currentTrack: null,
+            currentTrackIndex: 0,
+            transitionName: null
+        };
+    },
+    methods: {
+        play() {
+            if (this.audio.paused) {
+                this.audio.play();
+                this.isTimerPlaying = true;
+            } else {
+                this.audio.pause();
+                this.isTimerPlaying = false;
+            }
+        },
+        generateTime() {
+            let width = (100 / this.audio.duration) * this.audio.currentTime;
+            this.barWidth = width + "%";
+            this.circleLeft = width + "%";
+            let durmin = Math.floor(this.audio.duration / 60);
+            let dursec = Math.floor(this.audio.duration - durmin * 60);
+            let curmin = Math.floor(this.audio.currentTime / 60);
+            let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+            if (durmin < 10) {
+                durmin = "0" + durmin;
+            }
+            if (dursec < 10) {
+                dursec = "0" + dursec;
+            }
+            if (curmin < 10) {
+                curmin = "0" + curmin;
+            }
+            if (cursec < 10) {
+                cursec = "0" + cursec;
+            }
+            this.duration = durmin + ":" + dursec;
+            this.currentTime = curmin + ":" + cursec;
+        },
+        updateBar(x) {
+            let progress = this.$refs.progress;
+            let maxduration = this.audio.duration;
+            let position = x - progress.offsetLeft;
+            let percentage = (100 * position) / progress.offsetWidth;
+            if (percentage > 100) {
+                percentage = 100;
+            }
+            if (percentage < 0) {
+                percentage = 0;
+            }
+            this.barWidth = percentage + "%";
+            this.circleLeft = percentage + "%";
+            this.audio.currentTime = (maxduration * percentage) / 100;
+            this.audio.play();
+        },
+
+        clickProgress(e) {
+            this.isTimerPlaying = true;
+            this.audio.pause();
+            this.updateBar(e.pageX);
+        },
+        prevTrack() {
+            let progress = this.$refs.progress;
+            this.audio.currentTime -= 15;
+            this.audio.play();
+        },
+
+        nextTrack() {
+            let progress = this.$refs.progress;
+            this.audio.currentTime += 15;
+            this.audio.play();
+        },
+        resetPlayer() {
+            this.barWidth = 0;
+            this.circleLeft = 0;
+            this.audio.currentTime = 0;
+            this.audio.src = this.currentTrack.source;
+            setTimeout(() => {
+                if (this.isTimerPlaying) {
+                    this.audio.play();
+                } else {
+                    this.audio.pause();
+                }
+            }, 300);
+        },
+        favorite() {
+            this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+                this.currentTrackIndex
+            ].favorited;
+        }
+    },
+    created() {
+        let vm = this;
+        this.currentTrack = this.tracks[0];
+        this.audio = new Audio();
+        this.audio.src = this.currentTrack.source;
+        this.audio.ontimeupdate = function () {
+            vm.generateTime();
+        };
+        this.audio.onloadedmetadata = function () {
+            vm.generateTime();
+        };
+        this.audio.onended = function () {
+            vm.nextTrack();
+            this.isTimerPlaying = true;
+        };
+
+        // this is optional (for preload covers)
+        for (let index = 0; index < this.tracks.length; index++) {
+            const element = this.tracks[index];
+            let link = document.createElement('link');
+            link.rel = "prefetch";
+            link.href = element.cover;
+            link.as = "image"
+            document.head.appendChild(link)
+        }
+    }
+});
